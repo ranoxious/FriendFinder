aeb92eeefe7c37bff88835d5c3bfcb067edf30c6
Ranoxious <jumpship3456@gmail.com>
2018-08-17 21:06:38 -0400

    original

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+var users = [
+    {
+        name: "Totoro",
+        photo: "https://www.deviantart.com/clockwork2/art/My-Neighbor-Totoro-573701038pg?",
+        scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+    },
+    {
+        name: "Pucca",
+        photo: "http://images2.fanpop.com/image/photos/10200000/Pucca-pucca-10222952-495-369.jpg",
+        scores: [1, 5, 2, 3, 5, 4, 5, 4, 2, 1]
+    }
+];
+
+module.exports = users;
+
+
